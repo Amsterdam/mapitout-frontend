6f8c02e2ecc0964347fa2ae75205aeb28207a3cc
Alex Cazacu <alex@nvent.nl>
2019-04-11 19:23:17 +0200

    Implemented url navigation

--- src/main.js
+++ src/main.js
@@ -4,69 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import { isEqual } from "lodash-es";
 
 Vue.config.productionTip = false;
 
 sync(store, router);
-
-store.watch(
-  state => state.filters.filters,
-  filters => {
-    const selectedFilterIds = filters.filter(filter => filter.selected).map(filter => filter.id);
-
-    router.push({
-      query: {
-        ...router.currentRoute.query,
-        filters: selectedFilterIds.length > 0 ? selectedFilterIds.join(",") : undefined
-      }
-    });
-  }
-);
-
-store.watch(
-  state => state.ranges.ranges,
-  ranges => {
-    const definedRanges = ranges.filter(range => range.originLat && range.originLng);
-
-    router.push({
-      query: {
-        ...router.currentRoute.query,
-        range: definedRanges.length > 0 ? definedRanges.map(range => range.id) : undefined
-      }
-    });
-  }
-);
-
-router.beforeEach((to, from, next) => {
-  if (!isEqual(to.query, from.query)) {
-    try {
-      const queryFilterIds = to.query.filters.split(",").map(filterId => parseInt(filterId));
-      const storeFilterIds = store.state.filters.filters
-        .filter(filter => filter.selected)
-        .map(filter => filter.id);
-
-      if (!isEqual(queryFilterIds, storeFilterIds)) {
-        store.commit(
-          "filters/update",
-          store.state.filters.filters.map(filter => {
-            return { ...filter, selected: queryFilterIds.includes(filter.id) };
-          })
-        );
-      }
-
-      const ranges = to.query.range;
-
-      console.log(ranges);
-    } catch (e) {
-      console.log(e);
-    }
-
-    return next();
-  }
-
-  return next(false);
-});
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import RangesPanel from "./components/RangesPanel";
+import store from "./store";
+import qs from "qs";
 
 Vue.use(Router);
 
@@ -17,6 +19,39 @@ const router = new Router({
       component: RangesPanel
     }
   ]
+});
+
+router.onReady(async () => {
+  const url = new URL(window.location);
+
+  const ranges = Object.values(qs.parse(url.searchParams.get("ranges"))).map((range, index) => ({
+    ...range,
+    id: `range-${index}`,
+    travelTime: parseInt(range.travelTime),
+    originLat: parseFloat(range.originLat),
+    originLng: parseFloat(range.originLng)
+  }));
+
+  const selectedFilters = Object.values(qs.parse(url.searchParams.get("filters"))).map(filterId =>
+    parseInt(filterId)
+  );
+
+  const filters = store.state.filters.filters.map(filter => ({
+    ...filter,
+    selected: selectedFilters.includes(filter.id)
+  }));
+
+  store.replaceState({
+    ...store.state,
+    ranges: {
+      ...store.state.ranges,
+      ranges
+    },
+    filters: {
+      ...store.state.filters,
+      filters
+    }
+  });
 });
 
 export default router;
