a783b20a49239935a89c897a53576797aa8cbfa1
Alex Cazacu <alex@nvent.nl>
2019-04-16 11:41:00 +0200

    Updated DetailsPanel unit tests

--- tests/unit/components/DetailsPanel.spec.js
+++ tests/unit/components/DetailsPanel.spec.js
@@ -1,34 +1,48 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils";
+import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
+import Router from "vue-router";
 
 import DetailsPanel from "@/components/DetailsPanel.vue";
+import flushPromises from "flush-promises";
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+localVue.use(Router);
 
 describe("DetailsPanel", () => {
   const lookupSpy = jest.fn();
   const store = new Vuex.Store({
     modules: {
-      locations: {
+      pois: {
         namespaced: true,
-        state: {
-          viewing: null
-        },
         actions: {
           lookup: lookupSpy
         }
       }
     }
   });
-  const $router = {
-    back: jest.fn()
-  };
+  const router = new Router({
+    mode: "history",
+    base: "localhost",
+    routes: [
+      {
+        path: "/",
+        name: "home",
+        component: { template: "<div></div>" }
+      },
+      {
+        path: "/details/:poi",
+        name: "details",
+        component: DetailsPanel
+      }
+    ]
+  });
 
-  it("should create", () => {
+  it("should create", async () => {
     const wrapper = shallowMount(DetailsPanel, {
       localVue,
-      store
+      store,
+      router
     });
 
     expect(wrapper.isVueInstance()).toBeTruthy();
@@ -38,13 +52,81 @@ describe("DetailsPanel", () => {
     const wrapper = shallowMount(DetailsPanel, {
       localVue,
       store,
-      mocks: {
-        $router
-      }
+      router
     });
+    const backSpy = jest.spyOn(router, "back");
 
     wrapper.find(".back").trigger("click");
 
-    expect($router.back).toHaveBeenCalledTimes(1);
+    expect(backSpy).toHaveBeenCalledTimes(1);
+  });
+
+  // todo figure out how to test this
+  xdescribe("beforeRouteEnter", () => {
+    it("should call poi/lookup with the route poi param", async () => {
+      mount(
+        {
+          template: "<div><router-view /></div>"
+        },
+        {
+          localVue,
+          store,
+          router
+        }
+      );
+
+      router.push({ name: "details", params: { poi: "test" } });
+
+      await flushPromises();
+
+      expect(lookupSpy).toHaveBeenCalledTimes(1);
+      expect(lookupSpy).toHaveBeenCalledWith("test");
+    });
+  });
+
+  // todo figure out how to test this
+  xdescribe("beforeRouteUpdate", () => {
+    it("should call poi/lookup on change of the route poi param", async () => {
+      mount(
+        {
+          template: "<div><router-view /></div>"
+        },
+        {
+          localVue,
+          store,
+          router
+        }
+      );
+
+      router.push({ name: "details", params: { poi: "test" } });
+      await flushPromises();
+      lookupSpy.mockReset();
+
+      router.push({ name: "details", params: { poi: "test2" } });
+      await flushPromises();
+      expect(lookupSpy).toHaveBeenCalledTimes(1);
+      expect(lookupSpy).toHaveBeenCalledWith("test2");
+    });
+
+    it("should not call poi/lookup on change of the route poi param", async () => {
+      mount(
+        {
+          template: "<div><router-view /></div>"
+        },
+        {
+          localVue,
+          store,
+          router
+        }
+      );
+
+      router.push({ name: "details", params: { poi: "test" } });
+      await flushPromises();
+      lookupSpy.mockReset();
+
+      router.push({ name: "details", params: { poi: "test" } });
+      await flushPromises();
+      expect(lookupSpy).not.toHaveBeenCalled();
+    });
   });
 });
